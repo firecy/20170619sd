47b4c4076f6712a382a78f2a456079319b00335b
firecy <jiangdama@yeah.net>
2018-04-23 11:10:17 +0800

    add a inverse function in zca whitening

--- functions.py
+++ functions.py
@@ -6,8 +6,7 @@ import timeit
 from datetime import datetime
 import time
 
-import numpy
-from numpy import *
+import numpy as np
 
 def minmax_standardization(x, x_min, x_max):
     '''
@@ -28,8 +27,8 @@ def fea_standardization(x):
     this function realizes data feature standardization.
     The data is converted to a mean of 0 and variance data 1.
     '''
-    x -= numpy.mean(x, axis=0)
-    x /= numpy.std(x, axis=0)
+    x -= np.mean(x, axis=0)
+    x /= np.std(x, axis=0)
     return x
 
 def fea_standardization2(x, x_mean, x_std):
@@ -41,31 +40,26 @@ def fea_standardization2(x, x_mean, x_std):
     x /= x_std
     return x
 
+def fea_standardization_inverse(x, x_mean, x_std):
+    x *= x_std
+    x += x_mean
+    return x
+
 def get_usv(x):
     x = fea_standardization(x)
-    cov = numpy.dot(x.T, x) / x.shape[0]
-    u, s, v = numpy.linalg.svd(cov)
-    return u, s
+    cov = np.dot(x.T, x) / x.shape[0]
+    x_u, x_s, x_v = np.linalg.svd(cov)
+    return x_u, x_s
 
-def zca_whitening(x, epsilon):
+def zca_whitening(x, x_u, x_s, epsilon, x_mean, x_std):
     '''
     this function is aimed to reduce the relevance of data and noises.
     '''
-    cov = numpy.dot(x.T, x) / x.shape[0]
-    U, S, V = numpy.linalg.svd(cov)
-    xrot = numpy.dot(x, U)
-    xpcawhite = xrot / numpy.sqrt(S + epsilon)
-    xzcawhite = numpy.dot(xpcawhite, U.T)
-    return xzcawhite
-
-def zca_whitening2(x, u, s, epsilon):
-    '''
-    this function is aimed to reduce the relevance of data and noises.
-    '''
-    x -= numpy.mean(x, axis=0)
-    xrot = numpy.dot(x, u)
-    xpcawhite = xrot / numpy.sqrt(s + epsilon)
-    xzcawhite = numpy.dot(xpcawhite, u.T)
+    x -= np.mean(x, axis=0)
+    xrot = np.dot(x, x_u)
+    xpcawhite = xrot / np.sqrt(x_s + epsilon)
+    xzcawhite = np.dot(xpcawhite, x_u.T)
+    xzcawhite = fea_standardization_inverse(xzcawhite, x_mean, x_std)
     return xzcawhite
 
 def ts_ms(ts):
